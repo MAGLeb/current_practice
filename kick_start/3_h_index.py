3996ca0e0a8efd9ed3fcdc284797053e010b01e2
magleb <maximovglebigorevich@gmail.com>
2022-02-20 16:33:16 +0400

    finish h-index

--- kick_start/3_h_index.py
+++ kick_start/3_h_index.py
@@ -4,28 +4,25 @@ from typing import List
 
 def get_h_index(c: List[int]):
     result = [0]
-    d = {0: 1}
+    d = {0: 0}
+    counter = 0
 
     for i in range(len(c)):
         number = c[i]
-
-        if number in d:
-            d[number] += 1
-        else:
-            d[number] = 1
-
-        counter = 0
         last_elem = result[-1]
-        for key in sorted(d):
-            if key in d and key > last_elem:
-                counter += d[key]
+
+        if number > last_elem:
+            if number in d:
+                d[number] += 1
+            else:
+                d[number] = 1
+            counter += 1
 
         if counter > last_elem:
-            result.append(counter)
             if last_elem in d:
+                counter -= d[last_elem]
                 del d[last_elem]
-        else:
-            result.append(last_elem)
+        result.append(max(counter, last_elem))
 
     return result[1:]
 
