1f93545f3cc8d0e258de836cc3c7e44d9985be01
magleb <maximovglebigorevich@gmail.com>
2022-03-16 14:47:21 +0400

    euler 6 task

--- euler/6.cpp
+++ euler/6.cpp
@@ -1,3 +1,21 @@
-//
-// Created by magleb on 16.03.2022.
-//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  unsigned long long result;
+  u_long squared;
+  for(int i = 0; i <= 100; ++i) {
+    squared += i * i;
+    result += i
+  }
+
+  for(int i = 0; i <= 100; ++i) {
+
+  }
+
+  cout << squared << endl;
+  cout << result << endl;
+  cout << result * result << endl;
+
+}
