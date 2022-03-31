2e5ec9ee99c82e6a77a71a7d9a4a689b188f6ed1
magleb <maximovglebigorevich@gmail.com>
2022-03-31 21:57:54 +0400

    4 week

--- coursera/c++/yellow_belt/week_4/combinations.cpp
+++ coursera/c++/yellow_belt/week_4/combinations.cpp
@@ -1,3 +1,36 @@
-//
-// Created by magleb on 31.03.2022.
-//
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  bool first = true;
+  for (auto &val : v) {
+    if (first) {
+      first = false;
+      os << val;
+    } else {
+      os << " " << val;
+    }
+  }
+  return os;
+}
+
+void PrintCombinations(const int &n) {
+  vector<int> numbers;
+
+  for (int i = n; i > 0; --i)
+    numbers.push_back(i);
+
+  do {
+    cout << numbers << endl;
+  } while (prev_permutation(numbers.begin(), numbers.end()));
+}
+
+int main() {
+  int n;
+  cin >> n;
+  PrintCombinations(n);
+}
