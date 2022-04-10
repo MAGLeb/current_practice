f8e79849e549d6b09cd14e07be295b25867b3268
magleb <maximovglebigorevich@gmail.com>
2022-04-10 16:20:54 +0400

    coursera c++ week 4 finish

--- coursera/c++/yellow_belt/week_4/final.cpp
+++ coursera/c++/yellow_belt/week_4/final.cpp
@@ -2,4 +2,64 @@
 
 using namespace std;
 
-int main() {}
+void PrintExpressionWithout(const string &initial,
+                            const vector<char> &operations,
+                            const vector<string> &numbers,
+                            const int &initial_brackets) {
+  char prev_operation = ' ';
+  char operation = ' ';
+  int k = operations.size();
+
+  cout << string(initial_brackets, '(') << initial;
+
+  for (int i = 0; i < k; ++i) {
+    operation = operations[i];
+
+    if ((prev_operation == '+' || prev_operation == '-') &&
+        (operation == '*' || operation == '/')) {
+      cout << ')';
+    }
+    cout << ' ' << operation << ' ' << numbers[i];
+    prev_operation = operation;
+  }
+
+  cout << endl;
+}
+
+void PrintExpression(const string &initial, const vector<char> &operations,
+                     const vector<string> &numbers) {
+  int k = operations.size();
+  cout << string(k, '(') << initial << string(min(k, 1), ')');
+
+  for (int i = 0; i < k; ++i) {
+    cout << ' ' << operations[i] << ' ' << numbers[i];
+    if (i != k - 1)
+      cout << ')';
+  }
+  cout << endl;
+}
+
+int main() {
+  int initial, number, k;
+  int counter = 0;
+  char operation;
+  char prev_operation = ' ';
+  cin >> initial >> k;
+  vector<char> operations(k);
+  vector<string> numbers(k);
+
+  for (int i = 0; i < k; ++i) {
+    cin >> operation >> number;
+    operations[i] = operation;
+    numbers[i] = to_string(number);
+
+    if ((prev_operation == '+' || prev_operation == '-') &&
+        (operation == '*' || operation == '/')) {
+      counter++;
+    }
+    prev_operation = operation;
+  }
+
+  PrintExpression(to_string(initial), operations, numbers);
+//  PrintExpressionWithout(to_string(initial), operations, numbers, counter);
+}
--- coursera/c++/yellow_belt/week_5/final_check.cpp
+++ coursera/c++/yellow_belt/week_5/final_check.cpp
@@ -1,5 +1,111 @@
-#include <bits/stdc++.h>
+﻿#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main() {}
+class Human {
+public:
+
+  Human(string name) {
+    Name = name;
+    type = "someone";
+  }
+
+  virtual void Walk(const string& destination) const {
+    cout << "Someone: " << Name << " walks to: " << destination << endl;
+  }
+
+  string getType() const {
+    return type;
+  }
+
+  string getName() const {
+    return Name;
+  }
+
+protected:
+  string Name;
+  string type;
+};
+
+class Student : public Human {
+public:
+
+  Student(string name, string favouriteSong) : Human(name) {
+    FavouriteSong = favouriteSong;
+    type = "Student";
+  }
+
+  void Walk(const string& destination) const override {
+    cout << "Student " << Name << " walks to " << destination << endl;
+  }
+
+
+  void Learn() {
+    cout << "Student " << Name << " learns" << endl;
+  }
+
+  void SingSong() {
+    cout << "Student " << Name << " sings a song " << FavouriteSong << endl;
+  }
+
+public:
+  string FavouriteSong;
+};
+
+
+class Teacher : public Human {
+public:
+
+  Teacher(string name, string subject) : Human(name) {
+    Subject = subject;
+    type = "Teacher";
+  }
+
+  void Walk(const string& destination)const override {
+    cout << "Teacher " << Name << " walks to " << destination << endl;
+  }
+
+  void Teach() {
+    cout << "Teacher " << Name << " teaches " << Subject << endl;
+  }
+
+public:
+  string Subject;
+};
+
+
+class Policeman : public Human {
+public:
+  Policeman(string name) : Human(name) {
+    type = "Policeman";
+  }
+
+  void Walk(const string& destination) const override {
+    cout << "Policeman " << Name << " walks to " << destination << endl;
+  }
+
+  void Check(const Human& human) const {
+    cout << "Policeman " << Name << " checks " << human.getType() << "." << human.getType() << "'s name is " << human.getName() << endl;
+  }
+};
+
+
+void VisitPlaces(const Human& human, vector<string> places) {
+  for (const string& p : places) {
+    human.Walk(p);
+  }
+}
+
+int main() {
+  Teacher t("Jim", "Math");
+  Student s("Ann", "We will rock you");
+  Policeman p("Bob"), pp("Job");
+
+  VisitPlaces(t, {"Moscow", "London"});
+  p.Check(s);
+  p.Check(pp);
+  VisitPlaces(s, {"Moscow", "London"});
+  return 0;
+}
